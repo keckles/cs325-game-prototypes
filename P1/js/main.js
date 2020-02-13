dd27f06e7813be5608e16f4afbbdd7cdc79f6787
keckles <kecklesgame@gmail.com>
2020-02-12 23:17:01 -0500

    made project 1

--- P1/js/main.js
+++ P1/js/main.js
@@ -4,14 +4,15 @@ function make_main_game_state( game )
 {
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image('arrow', 'assets/Arrow.png');
     }
     
     var bouncy;
-    
+    var text;
     function create() {
+        game.physics.arcade.gravity.y = 100;
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'arrow' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
@@ -24,24 +25,30 @@ function make_main_game_state( game )
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        text = game.add.text(game.world.centerX, 15, "Fizzyness: ", style);
         text.anchor.setTo( 0.5, 0.0 );
     }
-    
+    var score = 0;
     function update() {
-        
         // Accelerate the 'logo' sprite towards the cursor,
         // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer(bouncy, game.input.activePointer, 500, 500, 500);
-        
+        if (game.input.activePointer.leftButton.isDown) {
+            bouncy.rotation = game.physics.arcade.accelerateToPointer(bouncy, game.input.activePointer, 10000, 500, 500);
+        } else {
+            game.physics.arcade.accelerateToPointer(bouncy, game.input.activePointer, 0, 500, 500);
+        }
+        score += Math.round((Math.abs(bouncy.body.velocity.x) + Math.abs(bouncy.body.velocity.y)) / 500);
+        if (score > 0) {
+            score--;
+        }
+        text.text = "Fizzyness: "+score;
     }
     
     return { "preload": preload, "create": create, "update": update };
 }
-
 
 window.onload = function() {
     // You might want to start with a template that uses GameStates:
@@ -56,9 +63,10 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
     
     game.state.add( "main", make_main_game_state( game ) );
     
     game.state.start( "main" );
 };
+
