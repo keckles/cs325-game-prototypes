de49dab9cd874bb3ae388037610261f50d0339ae
keckles <kecklesgame@gmail.com>
2020-04-23 17:54:52 -0400

    set up external server

--- P6/public/js/Game.js
+++ P6/public/js/Game.js
@@ -107,7 +107,7 @@ GameStates.makeGame = function( game, shared ) {
     
         create: function () {
             var self = this;
-            this.socket = io();
+            this.socket = io('10.0.0.140:8081');
             
 
             keys = {
--- P6/server.js
+++ P6/server.js
@@ -15,11 +15,9 @@ io.on('connection', function (socket) {
     console.log('a user connected');
     // create a new player and add it to our players object
     players[socket.id] = {
-        rotation: 0,
-        x: Math.floor(Math.random() * 700) + 50,
-        y: Math.floor(Math.random() * 500) + 50,
+        x: 0,
+        y: 0,
         playerId: socket.id,
-        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
     };
     // send the players object to the new player
     socket.emit('currentPlayers', players);
